55facc28045f8fb7fe70b79aeee82455ed5b18ac
szabotudor <szabo.tudor@gmail.com>
2026-03-18 23:22:49 +0100

    Fix warnings in SIMD for math131

--- crates/math/src/simd.rs
+++ crates/math/src/simd.rs
@@ -1,6 +1,6 @@
 use crate::Vec4f32;
 use std::{
-    arch::x86_64::{_mm_add_ps, _mm_div_ps, _mm_mul_ps, _mm_sub_ps},
+    arch::x86_64::{__m128, _mm_add_ps, _mm_div_ps, _mm_mul_ps, _mm_sub_ps},
     ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign},
 };
 
@@ -14,11 +14,11 @@ impl Add for Vec4f32 {
     #[inline(always)]
     fn add(self, rhs: Self) -> Self::Output {
         unsafe {
-            let lhs = std::mem::transmute(self.data);
-            let rhs = std::mem::transmute(rhs.data);
+            let lhs = std::mem::transmute::<[f32; 4], __m128>(self.data);
+            let rhs = std::mem::transmute::<[f32; 4], __m128>(rhs.data);
             let res = _mm_add_ps(lhs, rhs);
             Self {
-                data: std::mem::transmute(res),
+                data: std::mem::transmute::<__m128, [f32; 4]>(res),
             }
         }
     }
@@ -29,11 +29,11 @@ impl Sub for Vec4f32 {
     #[inline(always)]
     fn sub(self, rhs: Self) -> Self::Output {
         unsafe {
-            let lhs = std::mem::transmute(self.data);
-            let rhs = std::mem::transmute(rhs.data);
+            let lhs = std::mem::transmute::<[f32; 4], __m128>(self.data);
+            let rhs = std::mem::transmute::<[f32; 4], __m128>(rhs.data);
             let res = _mm_sub_ps(lhs, rhs);
             Self {
-                data: std::mem::transmute(res),
+                data: std::mem::transmute::<__m128, [f32; 4]>(res),
             }
         }
     }
@@ -44,11 +44,11 @@ impl Mul for Vec4f32 {
     #[inline(always)]
     fn mul(self, rhs: Self) -> Self::Output {
         unsafe {
-            let lhs = std::mem::transmute(self.data);
-            let rhs = std::mem::transmute(rhs.data);
+            let lhs = std::mem::transmute::<[f32; 4], __m128>(self.data);
+            let rhs = std::mem::transmute::<[f32; 4], __m128>(rhs.data);
             let res = _mm_mul_ps(lhs, rhs);
             Self {
-                data: std::mem::transmute(res),
+                data: std::mem::transmute::<__m128, [f32; 4]>(res),
             }
         }
     }
@@ -59,11 +59,11 @@ impl Div for Vec4f32 {
     #[inline(always)]
     fn div(self, rhs: Self) -> Self::Output {
         unsafe {
-            let lhs = std::mem::transmute(self.data);
-            let rhs = std::mem::transmute(rhs.data);
+            let lhs = std::mem::transmute::<[f32; 4], __m128>(self.data);
+            let rhs = std::mem::transmute::<[f32; 4], __m128>(rhs.data);
             let res = _mm_div_ps(lhs, rhs);
             Self {
-                data: std::mem::transmute(res),
+                data: std::mem::transmute::<__m128, [f32; 4]>(res),
             }
         }
     }
@@ -73,40 +73,40 @@ impl Div for Vec4f32 {
 impl AddAssign for Vec4f32 {
     fn add_assign(&mut self, rhs: Self) {
         unsafe {
-            let lhs = std::mem::transmute(self.data);
-            let rhs = std::mem::transmute(rhs.data);
+            let lhs = std::mem::transmute::<[f32; 4], __m128>(self.data);
+            let rhs = std::mem::transmute::<[f32; 4], __m128>(rhs.data);
             let res = _mm_add_ps(lhs, rhs);
-            self.data = std::mem::transmute(res);
+            self.data = std::mem::transmute::<__m128, [f32; 4]>(res);
         }
     }
 }
 impl SubAssign for Vec4f32 {
     fn sub_assign(&mut self, rhs: Self) {
         unsafe {
-            let lhs = std::mem::transmute(self.data);
-            let rhs = std::mem::transmute(rhs.data);
+            let lhs = std::mem::transmute::<[f32; 4], __m128>(self.data);
+            let rhs = std::mem::transmute::<[f32; 4], __m128>(rhs.data);
             let res = _mm_sub_ps(lhs, rhs);
-            self.data = std::mem::transmute(res);
+            self.data = std::mem::transmute::<__m128, [f32; 4]>(res);
         }
     }
 }
 impl MulAssign for Vec4f32 {
     fn mul_assign(&mut self, rhs: Self) {
         unsafe {
-            let lhs = std::mem::transmute(self.data);
-            let rhs = std::mem::transmute(rhs.data);
+            let lhs = std::mem::transmute::<[f32; 4], __m128>(self.data);
+            let rhs = std::mem::transmute::<[f32; 4], __m128>(rhs.data);
             let res = _mm_mul_ps(lhs, rhs);
-            self.data = std::mem::transmute(res);
+            self.data = std::mem::transmute::<__m128, [f32; 4]>(res);
         }
     }
 }
 impl DivAssign for Vec4f32 {
     fn div_assign(&mut self, rhs: Self) {
         unsafe {
-            let lhs = std::mem::transmute(self.data);
-            let rhs = std::mem::transmute(rhs.data);
+            let lhs = std::mem::transmute::<[f32; 4], __m128>(self.data);
+            let rhs = std::mem::transmute::<[f32; 4], __m128>(rhs.data);
             let res = _mm_div_ps(lhs, rhs);
-            self.data = std::mem::transmute(res);
+            self.data = std::mem::transmute::<__m128, [f32; 4]>(res);
         }
     }
 }
